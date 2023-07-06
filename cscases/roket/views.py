b7921ae37fb2c8dea1c7c67d19a17b602f92e1b2
Iudà <86917292+IUDA194@users.noreply.github.com>
2023-07-07 01:52:57 +0300

    Added error handling in rocket

--- cscases/roket/views.py
+++ cscases/roket/views.py
@@ -7,9 +7,18 @@ import json
 def lose_in_roket(request):
     if request.method == "GET":
         user_id = request.GET.get("user_id")
+        if user_id == None:
+            ansver = json.dumps({"error" : "'user_id' is not exist"})
+            return HttpResponse(ansver)
         amount = float(request.GET.get("amount"))
+        if amount == None:
+            ansver = json.dumps({"error" : "'amount' is not exist"})
+            return HttpResponse(ansver)
         user = User.objects.all().filter(user_id=user_id)
         old_balance = user[0].balance
+        if len(user) <= 0:
+            ansver = json.dumps({"error" : "No such user exists."})
+            return HttpResponse(ansver)
         user.update(balance=old_balance - amount)
         js_a = {"status" : "Done", "new_balance" : old_balance - amount, "lose_balance" : amount}
         ansver = json.dumps(js_a)
@@ -18,10 +27,21 @@ def lose_in_roket(request):
 def win_in_roket(request):
     if request.method == "GET":
         user_id = request.GET.get("user_id")
+        if user_id == None:
+            ansver = json.dumps({"error" : "'user_id' is not exist"})
+            return HttpResponse(ansver)
         amount = float(request.GET.get("amount"))
+        if amount == None:
+            ansver = json.dumps({"error" : "'amount' is not exist"})
+            return HttpResponse(ansver)
         factor = float(request.GET.get("factor"))
+        if factor == None:
+            ansver = json.dumps({"error" : "'factor' is not exist"})
+            return HttpResponse(ansver)
         user = User.objects.all().filter(user_id=user_id)
-        print(user[0].balance)
+        if len(user) <= 0:
+            ansver = json.dumps({"error" : "No such user exists."})
+            return HttpResponse(ansver)
         old_balance = user[0].balance
         user.update(balance=old_balance + (amount * factor) - amount)
         js_a = {"status" : "Done", "new_balance" : old_balance + (amount * factor), "win_balance" : amount * factor}
